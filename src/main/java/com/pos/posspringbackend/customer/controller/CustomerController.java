966356ca716d6b9eb12228c190e18c1bcc28fb03
CCS\calxa <xaviermbcalabia@gmail.com>
2023-07-05 13:46:40 +0800

    CustomerController added

---
+++ src/main/java/com/pos/posspringbackend/customer/controller/CustomerController.java
@@ -0,0 +1,50 @@
+package com.pos.posspringbackend.customer.controller;
+
+import com.pos.posspringbackend.customer.entity.Customer;
+import com.pos.posspringbackend.customer.service.CustomerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/customer")
+@CrossOrigin(origins = "localhost:4200")
+@RequiredArgsConstructor
+public class CustomerController {
+    private final CustomerService customerService;
+
+    @GetMapping
+    public ResponseEntity<List<Customer>> getAllCustomers() {
+        List<Customer> customers = customerService.getAllCustomers();
+        return new ResponseEntity<>(customers, HttpStatus.OK);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<Customer> create(@RequestBody Customer customer) {
+        Customer savedCustomer = customerService.createCustomer(customer);
+        return new ResponseEntity<>(savedCustomer, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Customer> findById(@PathVariable Integer id) {
+        Customer customer = customerService.findById(id);
+        return new ResponseEntity<>(customer, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Customer> update(
+            @PathVariable Integer id, @RequestBody Customer customer
+    ) {
+        Customer updatedCustomer = customerService.updateCustomer(id, customer);
+        return new ResponseEntity<>(updatedCustomer, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Boolean> delete (@PathVariable Integer id) {
+        boolean isDeleted = customerService.deleteCustomer(id);
+        return new ResponseEntity<>(isDeleted, HttpStatus.OK);
+    }
+}
