f50090d6811d90647b68e779e7dfd0d2f3d979d5
CCS\calxa <xaviermbcalabia@gmail.com>
2023-07-07 10:06:28 +0800

    Fix for Invoice

--- src/main/java/com/pos/posspringbackend/invoice/repository/InvoiceRepository.java
+++ src/main/java/com/pos/posspringbackend/invoice/repository/InvoiceRepository.java
@@ -1,7 +1,7 @@
 package com.pos.posspringbackend.invoice.repository;
 
 import com.pos.posspringbackend.invoice.document.InvoiceDocument;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface InvoiceRepository extends JpaRepository<InvoiceDocument, Integer> {
+public interface InvoiceRepository extends MongoRepository<InvoiceDocument, Integer> {
 }
